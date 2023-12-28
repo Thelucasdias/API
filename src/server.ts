9c640635fccf5661b9176ea49e95fe518b05e7fc
Thelucasdias <llucasdias@yahoo.com.br>
2023-12-28 11:36:46 -0300

    eslint installed and configured

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import fastify from 'fastify'
 const app = fastify()
 
 app.get('/hello', () => {
-    return 'Hello world'
+  return 'Hello world'
 })
 
-app.listen({
+app
+  .listen({
     port: 3000,
-}).then(() => {
+  })
+  .then(() => {
     console.log('listening on port: 3000')
-});
+  })
