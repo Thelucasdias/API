0207a0f89869359001f42dd9a77abaf402b6f5ed
Thelucasdias <llucasdias@yahoo.com.br>
2024-01-10 11:49:11 -0300

    init and config zod library

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,14 @@
 import { knex as setupKnex, Knex } from 'knex'
+import { env } from './env'
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is required')
+}
 
 export const config: Knex.Config = {
   client: 'sqlite3',
   connection: {
-    filename: './db/mydb.sqlite',
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
